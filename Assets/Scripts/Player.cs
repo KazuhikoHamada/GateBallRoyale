3ffd897da93e283bc93e88eeb127389662f1466d
k.hamada <k.hamadas55@gmail.com>
2021-12-30 18:21:19 +0900

    物理の挙動を修正する。

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UniRx;
+using UniRx.Triggers;
+using Cysharp.Threading.Tasks;
+using System;
 
 public class Player : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
@@ -11,22 +15,81 @@ public class Player : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IE
 
     private Vector2 _beginPosition;
 
+    private bool _isShot = false;
+
+
+    private float _preveMagnitude = 0f;
+
+    private float _maxMagnitude = 0f;
+
+    private float _stopMagnitude = 0.7f;
+
+    private float _delaySeconds = 3.0f;
+
+    private float _power = 5f;
+
+    private IDisposable _disposable = null;
+
+    private void Start()
+    {
+        this.OnCollisionEnter2DAsObservable().Where(_ => _disposable == null).Subscribe(_ => 
+        {
+            _disposable = GetMoveObservable();
+
+        }).AddTo(this);
+    }
+
+    private IDisposable GetMoveObservable()
+    {
+        return Observable.EveryFixedUpdate().DelaySubscription(TimeSpan.FromSeconds(_delaySeconds)).TakeWhile(_ => IsMoved()).Subscribe(_ =>
+        {
+            Debug.Log($"magnitude:{_rb.velocity.magnitude}, {_preveMagnitude - _rb.velocity.magnitude}");
+
+            _preveMagnitude = _rb.velocity.magnitude;
+
+        }, () =>
+        {
+            Debug.Log($"complete.");
+
+            _preveMagnitude = 0f;
+
+            _rb.velocity = Vector2.zero;
+
+            _isShot = false;
+
+            _disposable?.Dispose();
+            _disposable = null;
+        });
+    }
+
+    private bool IsMoved()
+    {
+        // falseになるとストップする
+        Debug.Log($"_preveMagnitude:{_preveMagnitude}, _rb.velocity.magnitude:{_rb.velocity.magnitude}, {_preveMagnitude - _rb.velocity.magnitude}");
+
+        //return _preveMagnitude - _rb.velocity.magnitude >= _stopMagnitude;
+        return _rb.velocity.magnitude >= _stopMagnitude;
+    }
+
+
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("OnPointerClick");
+        //Debug.Log("OnPointerClick");
 
-        _beginPosition = Position;
+        
     }
 
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("OnBeginDrag");
+        //Debug.Log("OnBeginDrag");
+
+        _beginPosition = Position;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("OnDrag");
+        //Debug.Log("OnDrag");
 
 
 
@@ -34,14 +97,40 @@ public class Player : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IE
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("OnEndDrag");
+        if (_isShot) return;
+
+        //Debug.Log($"IDisposable:{_disposable}");
 
         Vector2 targetPosition = Camera.main.ScreenToWorldPoint(eventData.position);
 
-        var a = (targetPosition - _beginPosition).normalized * -1f;
+        var direction = (targetPosition - _beginPosition).normalized * -1f;
 
-        _rb.AddForce(a * 10f, ForceMode2D.Impulse);
+        var distance = Vector2.Distance(targetPosition, _beginPosition);
+
+        Vector2 power = direction * distance * _power;
+
+        _preveMagnitude = (power / _rb.mass).magnitude;
+
+        Debug.Log($"OnEndDrag. {_preveMagnitude}");
+
+        _rb.AddForce(power, ForceMode2D.Impulse);
+
+        //_rb.AddForce(direction * distance * 10f);
+
+        _isShot = true;
+
+
+        _disposable?.Dispose();
+        _disposable = null;
+
+        _disposable = GetMoveObservable();
+
     }
 
+    private void OnDestroy()
+    {
+        _disposable?.Dispose();
+        _disposable = null;
+    }
 
 }
